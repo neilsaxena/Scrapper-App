e6375a5f6d387de1589337c8234a662ba7b4f97f
Neil Saxena <neilsaxena0509@gmail.com>
2021-12-30 13:49:25 +0530

    Changed selenium scrapping method

--- scrapperapp/routes.py
+++ scrapperapp/routes.py
@@ -1,3 +1,4 @@
+# Importing Libraries
 from flask import render_template, request
 from scrapperapp import app
 from selenium import webdriver
@@ -9,12 +10,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
+# Defining default route
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-
+# Route for displaying scrapped data
 @app.route("/prediction", methods=['POST'])
 def prediction():
     url_input = request.form["urlinput"]
@@ -23,22 +25,19 @@ def prediction():
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     driver.maximize_window()
 
-    temp_paragraphs = []
     paragraphs = []
 
+    # Opening given url in web browser
     driver.get(url_input)
+    # Waiting for the page to load properly in browser
     time.sleep(5)
-    elements = driver.find_elements_by_tag_name('body')
-    paragraphs = []
-    for element in elements:
-        paragraph = element.text
-        paragraphs = paragraph.split("\n")
-        break
+    # Scrapping text content
+    element = driver.find_element_by_tag_name('body')
+    scrapped_body = element.text
     driver.close()
-
+    paragraphs = scrapped_body.split("\n")
 
     # Check for empty sentence
-
     scrapped_sentences = []
     for paragraph in paragraphs:
         if paragraph.strip() != '':
